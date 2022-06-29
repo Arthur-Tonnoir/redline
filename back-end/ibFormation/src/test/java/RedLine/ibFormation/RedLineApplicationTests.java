4ef3fde4cb06c9adb098ae82ae1ae6f74010ce21
Maria <kanyalaekie@gmail.com>
2022-06-29 10:39:26 +0200

    chore: Ajout dans le dossier back-end contenant le projet généré par spring initializer

---
+++ back-end/ibFormation/src/test/java/RedLine/ibFormation/RedLineApplicationTests.java
@@ -0,0 +1,13 @@
+package RedLine.ibFormation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedLineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
