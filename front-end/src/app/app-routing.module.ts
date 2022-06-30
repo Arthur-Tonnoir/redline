4cf49eabf92d96dc24cab3d06ff06b1bc12ef725
etranger2008 <etranger2008@yahoo.com>
2022-07-01 01:08:51 +0200

    chore: les pages home,catalogue,list-formation et detail-formation realise avec minimal routing entre les pages,version scate a verifier,npm 8.5.5,node 16.15.0,angular/cli 14.0.3,angular-material 14.0.3,bootstrap5 + deux scripts a ajouter dans angular.json

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotfoundComponent } from './commons/notfound/notfound.component';
+import { DetailFormationComponent } from './pages/detail-formation/detail-formation.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ListFormationComponent } from './pages/list-formation/list-formation.component';
+import { CatalogueComponent } from './pages/catalogue/catalogue.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'home',pathMatch:'full'},
+  {path: 'home', component:HomeComponent},
+  {path: 'catalogue', component:CatalogueComponent},
+  {path: 'detailformation/:id', component:DetailFormationComponent},
+  {path: 'listformation', component:ListFormationComponent},
+  {path: '**', component:NotfoundComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
