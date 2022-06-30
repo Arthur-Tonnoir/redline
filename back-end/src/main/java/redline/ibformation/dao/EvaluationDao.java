b17c483a5f66ff1b18fe83c333bc27207fa260a5
Arthur TONNOIR <76939388+Arthur-Tonnoir@users.noreply.github.com>
2022-06-30 10:05:00 +0200

    Revert "Manipulation de la table Adresse fonctionne"

--- back-end/src/main/java/redline/ibformation/dao/EvaluationDao.java
+++
@@ -1,9 +0,0 @@
-package redline.ibformation.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import redline.ibformation.entity.Evaluation;
-
-@Repository
-public interface EvaluationDao extends JpaRepository<Evaluation, Long> {
-}
--- back-end/src/main/java/redline/ibformation/dao/ThemeDao.java
+++
@@ -1,8 +0,0 @@
-package redline.ibformation.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import redline.ibformation.entity.Theme;
-@Repository
-public interface ThemeDao extends JpaRepository<Theme, Long> {
-}
