039b9b9e618a49b891eb3cb1810b59036366868e
Arthur-Tonnoir <arthur1.tonnoir@sfr.fr>
2022-07-07 11:14:30 +0200

    fix: Correction de la variable formation pour recuperer la liste des Formation par soustheme :bug:

--- front-end/src/app/models/soustheme.model.ts
+++ front-end/src/app/models/soustheme.model.ts
@@ -1,7 +1,9 @@
+import Formation from "./formation.model";
 import Theme from "./theme.model";
 
 export default interface SousTheme extends Theme {
   id: number;
   nomSousTheme: string;
   theme: Theme;
+  formations: Formation[];
 }
