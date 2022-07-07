8118381b6008104e9a2007ee00ad59bafcb1eec9
Maria <kanyalaekie@gmail.com>
2022-07-07 12:05:42 +0200

    feat: Ajout du formulaire d'enregistrement à une session

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { DetailFormationComponent } from './pages/detail-formation/detail-format
 import { HomeComponent } from './pages/home/home.component';
 import { ListFormationComponent } from './pages/list-formation/list-formation.component';
 import { CatalogueComponent } from './pages/catalogue/catalogue.component';
+import { EnregSessionComponent } from './pages/enreg-session/enreg-session.component';
 
 const routes: Routes = [
   {path:'',redirectTo:'home',pathMatch:'full'},
@@ -12,6 +13,7 @@ const routes: Routes = [
   {path: 'catalogue', component:CatalogueComponent},
   {path: 'detailformation/:id', component:DetailFormationComponent},
   {path: 'listformation', component:ListFormationComponent},
+  {path: 'enregsession',component:EnregSessionComponent},
   {path: '**', component:NotfoundComponent}];
 
 @NgModule({
