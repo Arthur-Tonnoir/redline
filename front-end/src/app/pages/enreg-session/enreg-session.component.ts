8e76c40e3d2476bbb0188eb76458162ee63b19a8
Ekie maria goretti kanyala <kanyalaekie@gmail.com>
2022-07-10 22:56:20 +0200

    chore: Enregistrement d'un utilisateur dans la base de données en cours de réalisation

--- front-end/src/app/pages/enreg-session/enreg-session.component.ts
+++ front-end/src/app/pages/enreg-session/enreg-session.component.ts
@@ -10,43 +10,45 @@ import { UtilisateurService } from 'src/app/services/utilisateur.service';
 })
 export class EnregSessionComponent implements OnInit {
   utilisateur!: Utilisateur;
-  utilisateurForm: FormGroup;
+  //utilisateurForm: FormGroup;
+  submitted: boolean = false;
+  utilisateurForm: FormGroup = this.formBuilder.group({
+    nom: ['', [Validators.minLength(2), Validators.required]],
+    prenom: ['', [Validators.minLength(2), Validators.required]],
+    email: ['', [Validators.email, Validators.required]],
+    telephone: ['', [Validators.minLength(10), Validators.required]],
+    adresse: this.formBuilder.group({
+      numero: ['', [Validators.minLength(1), Validators.required]],
+      rue: ['', [Validators.minLength(2), Validators.required]],
+      complement: [''],
+      codePostal: ['', [Validators.minLength(5), Validators.required]],
+      ville: ['', [Validators.minLength(2), Validators.required]],
+    }),
+    fonctionResponsable: [''],
+    serviceAssocie: [''],
+    nomEntreprise: [''],
+    idSession: [''],
+    experience: [''],
+    noteFormateur: [''],
+    estClient: [true],
+    estFormateur: [false],
+    estResponsable: [false]
+  });
   constructor(private formBuilder: FormBuilder, private utilisateurService: UtilisateurService) {
-    this.utilisateurForm = this.formBuilder.group({
-      nom: ['', [Validators.minLength(2), Validators.required]],
-      prenom: ['', [Validators.minLength(2), Validators.required]],
-      email: ['', [Validators.email, Validators.required]],
-      telephone: ['', [Validators.minLength(10), Validators.required]],
-      adresse: this.formBuilder.group({
-        numero: ['', [Validators.minLength(1), Validators.required]],
-        rue: ['', [Validators.minLength(2), Validators.required]],
-        complement: [''],
-        codePostal: ['', [Validators.minLength(5), Validators.required]],
-        ville: ['', [Validators.minLength(2), Validators.required]],
-      }),
-      fonctionResponsable: [''],
-      serviceAssocie: [''],
-      nomEntreprise: [''],
-      idSession: [''],
-      experience: [''],
-      noteFormateur: [''],
-      estClient: [''],
-      estFormateur: [''],
-      estResponsable: ['']
-    });
+
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
 
-  ajoutUtilisateur(){
-    this.utilisateur.fonctionResponsable= '';
-    this.utilisateur.serviceAssocie= '';
-    this.utilisateur.nomEntreprise= '';
-    this.utilisateur.idSession= 1;
-    this.utilisateur.experience= 0;
-    this.utilisateur.noteFormateur= 0;
-    this.utilisateur.estClient= true;
-    this.utilisateur.estFormateur= false;
-    this.utilisateur.estResponsable= false;
+  }
+
+  get form() {
+    return this.utilisateurForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    console.log("envoyé");
+    this.utilisateurService.createUtilisateur(this.utilisateurForm.value);
   }
 }
