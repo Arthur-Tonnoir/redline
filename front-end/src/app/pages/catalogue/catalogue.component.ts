5209fe2b2f023034b36a90b74cf20a32c3cdd783
Arthur-Tonnoir <arthur1.tonnoir@sfr.fr>
2022-07-11 11:31:05 +0200

    feat: Intégration du back-end au pages ListFormation, Catalogue et DetailFormation :sparkles:

--- front-end/src/app/pages/catalogue/catalogue.component.ts
+++ front-end/src/app/pages/catalogue/catalogue.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import Domaine from 'src/app/models/domaine.model';
+import Theme from 'src/app/models/theme.model';
+import { DomaineService } from 'src/app/services/domaine.service';
+import { ThemeService } from 'src/app/services/theme.service';
 
 @Component({
   selector: 'app-catalogue',
@@ -7,9 +11,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CatalogueComponent implements OnInit {
 
-  constructor() { }
+  domaines: Domaine[] = [];
+
+  themes: Theme[]= [];
+
+  constructor(private dm : DomaineService, private tm : ThemeService) { }
 
   ngOnInit(): void {
+    this.dm.getDomaines().subscribe((domain) => {this.domaines = domain});
+    this.tm.getThemes().subscribe((theme) => {this.themes = theme});
   }
 
 }
